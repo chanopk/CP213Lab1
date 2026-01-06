4298ecc383d99adcb21ebf78bc0b9396bdd4d285
ChanopKhonnasee <101390166+chanopk@users.noreply.github.com>
2026-01-06 16:22:39 +0700

    chnage hello

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "ชนพ ดินแดง",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
